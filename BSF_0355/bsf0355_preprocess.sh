4f761e11323e8d6dd74394d88a983208a8d0ce2d
luenling <lukasendler@gmail.com>
2017-09-25 11:24:40 +0200

    changed preprocess script for run 355

--- BSF_0355/bsf0355_preprocess.sh
+++ BSF_0355/bsf0355_preprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # author Lukas Endler
 # created: 2017-09-24T13:54:09.196+02:00
-# Changed:
+# Changed: 9/25/2017, 11:21:52 AM
 # takes a bam file of reads with the BSF format BSF_##__HL37TBBXX_6_S_44_0_S28607.bam and trimmes and clips to get 2 fq.gz files
 # turn on extended globbing behavior
 shopt -s extglob
@@ -27,47 +27,53 @@ OUT=$FN
 
 LOGFILE=$OUT.log
 ERRORFILE=$OUT.err.log
-TRIMLOG=$OUT.trim.log
-OUTDIR=''
+#TRIMLOG=$OUT.trim.log
+OUTDIR='FASTQ/'
 if [ $2 ]; then
     OUTDIR=$2'/'
-    if [ ! -d $2 ]; then mkdir $3 ; fi
+    if [ ! -d $2 ]; then mkdir $2 ; fi
 fi
 
-echo converting bam to fastq at `date` >> ${LOGFILE}
-echo java -Xmx4g -jar ${PICARD} SamToFastq VALIDATION_STRINGENCY=SILENT INPUT=$INFILE FASTQ=${OUTDIR}${OUT}_1.fastq SECOND_END_FASTQ=${OUTDIR}${OUT}_2.fastq \>\> ${LOGFILE} 2\> ${ERRORFILE} >> ${LOGFILE}
-java -Xmx4g -jar ${PICARD} SamToFastq VALIDATION_STRINGENCY=SILENT INPUT=$INFILE FASTQ=${OUTDIR}${OUT}_1.fastq SECOND_END_FASTQ=${OUTDIR}${OUT}_2.fastq  >> ${LOGFILE} 2>> ${ERRORFILE}
-ES=$?
-echo finished at `date` with exit state $ES >> $LOGFILE
-[ $ES -eq 0 ] || exit $ES
-echo gzipping fastq ${OUTDIR}${OUT}_1.fastq at `date` >> ${LOGFILE}
-gzip -fq ${OUTDIR}${OUT}_1.fastq  >> ${LOGFILE} 2>> ${ERRORFILE}
-ES=$?
-echo finished at `date` with exit state $ES >> $LOGFILE
-[ $ES -eq 0 ] || exit $ES
-echo gzipping fastq ${OUTDIR}${OUT}_2.fastq at `date` >> ${LOGFILE}
-gzip -fq ${OUTDIR}${OUT}_2.fastq >> ${LOGFILE} 2>> ${ERRORFILE}
-ES=$?
-echo finished at `date` with exit state $ES >> $LOGFILE
-[ $ES -eq 0 ] || exit $ES
+OUT=${OUTDIR}${OUT}
 
-INF1=${OUTDIR}${OUT}_1.fastq.gz
-INF2=${OUTDIR}${OUT}_2.fastq.gz
+# echo converting bam to fastq at `date` >> ${LOGFILE}
+# echo java -Xmx4g -jar ${PICARD} SamToFastq VALIDATION_STRINGENCY=SILENT INPUT=$INFILE FASTQ=${OUTDIR}${OUT}_1.fastq SECOND_END_FASTQ=${OUTDIR}${OUT}_2.fastq \>\> ${LOGFILE} 2\> ${ERRORFILE} >> ${LOGFILE}
+# java -Xmx4g -jar ${PICARD} SamToFastq VALIDATION_STRINGENCY=SILENT INPUT=$INFILE FASTQ=${OUTDIR}${OUT}_1.fastq SECOND_END_FASTQ=${OUTDIR}${OUT}_2.fastq  >> ${LOGFILE} 2>> ${ERRORFILE}
+# ES=$?
+# echo finished at `date` with exit state $ES >> $LOGFILE
+# [ $ES -eq 0 ] || exit $ES
+# echo gzipping fastq ${OUTDIR}${OUT}_1.fastq at `date` >> ${LOGFILE}
+# gzip -fq ${OUTDIR}${OUT}_1.fastq  >> ${LOGFILE} 2>> ${ERRORFILE}
+# ES=$?
+# echo finished at `date` with exit state $ES >> $LOGFILE
+# [ $ES -eq 0 ] || exit $ES
+# echo gzipping fastq ${OUTDIR}${OUT}_2.fastq at `date` >> ${LOGFILE}
+# gzip -fq ${OUTDIR}${OUT}_2.fastq >> ${LOGFILE} 2>> ${ERRORFILE}
+# ES=$?
+# echo finished at `date` with exit state $ES >> $LOGFILE
+# [ $ES -eq 0 ] || exit $ES
+
+# INF1=${OUTDIR}${OUT}_1.fastq.gz
+# INF2=${OUTDIR}${OUT}_2.fastq.gz
 
 
 
 echo trimming and removing adaptors at `date` >> ${LOGFILE}
-echo $BBDUK -Xmx4g in1=$INF1 in2=$INF2 out1=${OUT}_npa_1.fq.gz out2=${OUT}_npa_2.fq.gz qtrim=r minlen=75 ktrim=r ref=\"$ADAPTS\" k=20 mink=15 trimq=15 overwrite=t tbo=t tpe=t \>\> ${LOGFILE} 2\> ${ERRORFILE} >> ${LOGFILE}
-$BBDUK -Xmx4g in1=$INF1 in2=$INF2 out1=${OUT}_npa_1.fq.gz out2=${OUT}_npa_2.fq.gz qtrim=r minlen=55 ktrim=r ref=\"$ADAPTS\" k=20 mink=15 trimq=15 overwrite=t tbo=t tpe=t >> ${LOGFILE} 2> ${ERRORFILE}
+echo $SAMTOOLS fastq $1 | $BBDUK -Xmx4g in=stdin.fq out1=${OUT}_npa_1.fq.gz out2=${OUT}_npa_2.fq.gz qtrim=r minlen=75 ktrim=r ref=\"$ADAPTS\" k=20 mink=15 trimq=15 overwrite=t tbo=t tpe=t \>\> ${LOGFILE} 2\> ${ERRORFILE} >> ${LOGFILE}
+$SAMTOOLS fastq $1 | $BBDUK -Xmx4g in=stdin.fq out1=${OUT}_npa_1.fq.gz out2=${OUT}_npa_2.fq.gz qtrim=r minlen=55 ktrim=r ref=\"$ADAPTS\" k=20 mink=15 trimq=15 overwrite=t tbo=t tpe=t >> ${LOGFILE} 2> ${ERRORFILE}
 ES=$?
 echo finished at `date` with exit state $ES >> $LOGFILE
 [ $ES -eq 0 ] || exit $ES
-if [ ! -d fastqc ]; then mkdir fastqc ; fi
-fastqc -q -o fastqc ${OUT}_npa_1.fq.gz ${OUT}_npa_2.fq.gz ${OUT}_np_1.fq.gz ${OUT}_np_2.fq.gz &
-if [ ! -d Merged ]; then mkdir Merged ; fi
-echo merging overlapping reads at `date` >> ${LOGFILE}
-echo $BBMERGE -Xmx4g  in1=${OUT}_npa_1.fq.gz in2=${OUT}_npa_2.fq.gz out=Merged/${OUT}_m.fq.gz outu1=Merged/${OUT}_m_1.fq.gz outu2=Merged/${OUT}_m_2.fq.gz strict=t qtrim2=r trimq=10 ouq=t ihist=Merged/${OUT}_m.hist 2\> Merged/${OUT}_m.log >>  $LOGFILE
-$BBMERGE -Xmx4g  in1=${OUT}_npa_1.fq.gz in2=${OUT}_npa_2.fq.gz out=Merged/${OUT}_m.fq.gz outu1=Merged/${OUT}_m_1.fq.gz outu2=Merged/${OUT}_m_2.fq.gz strict=t qtrim2=r trimq=10 ouq=t ihist=Merged/${OUT}_m.hist 2> Merged/${OUT}_m.log
-ES=$?
-echo finished at `date` with exit state $ES >> $LOGFILE
-[ $ES -eq 0 ] || exit $ES
+if [ ! -d ${OUTDIR}/fastqc ]; then mkdir ${OUTDIR}/fastqc ; fi
+fastqc -q -o ${OUTDIR}/fastqc ${OUT}_npa_1.fq.gz ${OUT}_npa_2.fq.gz &
+
+
+#
+#
+# if [ ! -d Merged ]; then mkdir Merged ; fi
+# echo merging overlapping reads at `date` >> ${LOGFILE}
+# echo $BBMERGE -Xmx4g  in1=${OUT}_npa_1.fq.gz in2=${OUT}_npa_2.fq.gz out=Merged/${OUT}_m.fq.gz outu1=Merged/${OUT}_m_1.fq.gz outu2=Merged/${OUT}_m_2.fq.gz strict=t qtrim2=r trimq=10 ouq=t ihist=Merged/${OUT}_m.hist 2\> Merged/${OUT}_m.log >>  $LOGFILE
+# $BBMERGE -Xmx4g  in1=${OUT}_npa_1.fq.gz in2=${OUT}_npa_2.fq.gz out=Merged/${OUT}_m.fq.gz outu1=Merged/${OUT}_m_1.fq.gz outu2=Merged/${OUT}_m_2.fq.gz strict=t qtrim2=r trimq=10 ouq=t ihist=Merged/${OUT}_m.hist 2> Merged/${OUT}_m.log
+# ES=$?
+# echo finished at `date` with exit state $ES >> $LOGFILE
+# [ $ES -eq 0 ] || exit $ES
