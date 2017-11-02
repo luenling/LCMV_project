e6206772b13a2a62b416163901f653c999be981c
Lukas Endler <lukasendler@i122mc94.vu-wien.ac.at>
2017-11-02 12:14:05 +0100

    typoooo

--- BSF_0355/strand_specific_depths.sh
+++ BSF_0355/strand_specific_depths.sh
@@ -9,7 +9,7 @@ source $(dirname $BASH_SOURCE)"/bsf_0355_params.sh"
 mkdir -p Coverages
 
 while read i ; do
-  SM=$($SAMTOOLS view -H BSF_0355_S01_real_viterbi_IDQS_bqsr.bam | grep '^@RG' | sed "s/.*SM:\([^\t]*\).*/\1/g")
+  SM=$($SAMTOOLS view -H $i | grep '^@RG' | sed "s/.*SM:\([^\t]*\).*/\1/g")
   $SAMTOOLS view -q 1 -h -F 256 -f 2 -G 96  $i  | $SAMTOOLS view -b -G 144 -  | $SAMTOOLS depth -m 100000000 -a - > Coverages/${SM}_plus.depth
   $SAMTOOLS view -q 1 -h -F 256 -f 2 -G 80  $i  | $SAMTOOLS view -b -G 160 -  | $SAMTOOLS depth -m 100000000 -a - > Coverages/${SM}_minus.depth
 done < $1
