c4e4c20b2c9570aaa65c73c0ab84eb1bd189af20
Lukas Endler <lukasendler@i122mc94.vu-wien.ac.at>
2017-11-02 12:17:18 +0100

    blub

--- BSF_0355/strand_specific_depths.sh
+++ BSF_0355/strand_specific_depths.sh
@@ -10,20 +10,21 @@ mkdir -p Coverages
 
 while read i ; do
   SM=$($SAMTOOLS view -H $i | grep '^@RG' | sed "s/.*SM:\([^\t]*\).*/\1/g")
+  echo $SM $i
   $SAMTOOLS view -q 1 -h -F 256 -f 2 -G 96  $i  | $SAMTOOLS view -b -G 144 -  | $SAMTOOLS depth -m 100000000 -a - > Coverages/${SM}_plus.depth
   $SAMTOOLS view -q 1 -h -F 256 -f 2 -G 80  $i  | $SAMTOOLS view -b -G 160 -  | $SAMTOOLS depth -m 100000000 -a - > Coverages/${SM}_minus.depth
 done < $1
 
-for i in *_plus.depth; do
+for i in Coverages/*_plus.depth; do
   A=$(basename $i _plus.depth);
-  echo $A | cat - <(cut -f 3 $i) | paste  bsf_${RUN_ID}_all_plus.depths - > temp;
-  mv -f bsf_${RUN_ID}_all_plus.depths;
+  echo $A | cat - <(cut -f 3 $i) | paste  Coverages/bsf_${RUN_ID}_all_plus.depths - > temp;
+  mv -f temp Coverages/bsf_${RUN_ID}_all_plus.depths;
 done
 
-for i in *_minus.depth; do
+for i in Coverages/*_minus.depth; do
   A=$(basename $i _minus.depth);
-  echo $A | cat - <(cut -f 3 $i) | paste  bsf_${RUN_ID}_all_minus.depths - > temp;
-  mv -f bsf_${RUN_ID}_all_minus.depths;
+  echo $A | cat - <(cut -f 3 $i) | paste  Coverages/bsf_${RUN_ID}_all_minus.depths - > temp;
+  mv -f temp Coverages/bsf_${RUN_ID}_all_minus.depths;
 done
 
 exit
