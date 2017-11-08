0758649c09f6351764f56eda10796dfd8df940c0
lukas endler <lukasendler@gmail.com>
2017-11-08 13:36:02 +0100

    refined breseq output

--- BSF_0355/call_breseq.sh
+++ BSF_0355/call_breseq.sh
@@ -24,10 +24,15 @@ while read file; do
   echo finished breseq at `date` with exit state $ES >> $LOGFILE
   echo $GDTOOLS gd2vcf -r $REFGENOME -r ${REFGENOME/%\.fasta/\.gff3} -o ${BN}.vcf output/output.gd >> $LOGFILE
   $GDTOOLS gd2vcf -r $REFGENOME -r ${REFGENOME/%\.fasta/\.gff3} -o ${BN}.vcf output/output.gd
-  awk -v OFS="\t" '/^\#\#[^I]/ {print} /^\#\#INFO/ {print $0; sub("INFO","FORMAT",$0); print $0; } /\#CH/ {print $0,"FORMAT","'$SM'"} !/^\#/ {form=$8;  gsub(/=[^A-Z]+/,":",form); gsub(/;/,":",form); sub(/:$/,"",form); sub(/INDEL:/,"",form);  samp=$8; gsub(/[A-Z4]+=/,"",samp); gsub(/;/,":",samp); sub(/INDEL:/,"",samp); print $0,form,samp}' ${BN}.vcf | $BCFTOOLS norm -f $REFGENOME - 2>> $ERRLOG | bgzip -c > ${BN}_samp_breseq.vcf.gz
+  awk -v OFS="\t" '/^\#\#[^I]/ {print} /^\#\#INFO/ {print $0; sub("INFO","FORMAT",$0); print $0; } /\#CH/ {print "##INFO=<ID=PQ,Number=1,Type=Float,Description=\"Breseq variant quality score (log10(pvariant/Pn)-log10(total length or references))\">"; print $0,"FORMAT","'$SM'"} !/^\#/ {form=$8;  gsub(/=[^A-Z]+/,":",form); gsub(/;/,":",form); sub(/:$/,"",form); sub(/INDEL:/,"",form);  samp=$8; gsub(/[A-Z4]+=/,"",samp); gsub(/;/,":",samp); sub(/INDEL:/,"",samp); print $0,form":PQ",samp":"$6}' ${BN}.vcf | $BCFTOOLS norm -f $REFGENOME - 2>> $ERRLOG | bgzip -c > ${BN}_samp_breseq.vcf.gz
   tabix -p vcf ${BN}_samp_breseq.vcf.gz
   cd ..
 done < $1
+
+gdtools COMPARE -r $REFGENOME -r ${REFGENOME/%\.fasta/\.gff3} -o all_samp_breseq_comp.html --repeat-header 25 `ls bsf_${RUN_ID}_S*/output/output.gd`
+
+gdtools COMPARE -r $REFGENOME -r ${REFGENOME/%\.fasta/\.gff3} -o all_samp_breseq_comp.tsv -f TSV `ls bsf_${RUN_ID}_S*/output/output.gd`
+
 
 $BCFTOOLS merge -i "DP:sum,AF:max,AD:sum"  -m none -O v "bsf_"*/*_samp_breseq.vcf.gz > all_samp_breseq.vcf
 
@@ -40,3 +45,30 @@ then
   tabix -f -p vcf all_samp_tmp.vcf.gz
   $BCFTOOLS annotate -a $DPS -c "+FORMAT/DP" all_samp_tmp.vcf.gz > all_samp_breseq.vcf
 fi
+
+for i in 0.1 0.05 0.001 ; do
+  LFVCF=all_samp_breseq_${i}.vcf
+  $BCFTOOLS view -i "AF>$i" all_samp_breseq.vcf | $BCFTOOLS norm -f $REFGENOME -m+any - > $LFVCF
+  FN=`basename $LFVCF .vcf`
+  LOGFILE=${FN}.log
+  ERRORLOG=${FN}.err.log
+  # run snpeff
+  echo starting snpeff at `date` >> $LOGFILE
+  echo  $SNPEFF -c $SNPEFF_CONF -dataDir $SNPEFF_DATA lcmv -no-intergenic -no "INTRAGENIC" -no-downstream -no-upstream -stats ${FN}_snpeff_log.html $LFVCF \> ${FN}_snpeff.vcf >> $LOGFILE
+  $SNPEFF  -c $SNPEFF_CONF -dataDir $SNPEFF_DATA lcmv -no-intergenic  -no "INTRAGENIC" -no-downstream -no-upstream -stats ${FN}_snpeff_log.html $LFVCF > ${FN}_snpeff.vcf
+  ES=$?
+  echo finished snpeff at `date` with exit state $ES >> $LOGFILE
+  [ $ES -eq 0 ] || exit $ES
+
+  # extract annotations and combine them with AFs and statistics
+  $SNPSIFT extractFields ${FN}_snpeff.vcf CHROM POS REF "ANN[0].GENE" "ANN[0].ALLELE" "ANN[0].EFFECT" "ANN[0].AA" "ANN[1].ALLELE" "ANN[1].EFFECT" "ANN[1].AA" | sed ' s/DQ361065\.7/NP/g; s/DQ361065\.4/GP/g; s/DQ361066\.4/geneZ/g ; s/DQ361066\.7/geneL/g; s/\[[0-9]*\]//g' > ${FN}_snpeff.tab
+  $BCFTOOLS query -Hf "%CHROM\t%POS\t%REF\t%ALT[\t%AD\t%DP\t%AF]\n"  $LFVCF > ${FN}.stats.tab
+  $BCFTOOLS query -Hf "%CHROM\t%POS\t%REF\t%ALT[\t%AF]\n"  $LFVCF > ${FN}.afs.tab
+  $BCFTOOLS query -Hf "%CHROM\t%POS\t%REF\t%ALT[\t%DP]\n"  $LFVCF > ${FN}.dp.tab
+  $BCFTOOLS query -Hf "%CHROM\t%POS\t%REF\t%ALT[\t%PQ]\n"  $LFVCF > ${FN}.pq.tab
+  paste ${FN}.afs.tab <(cut -f5- ${FN}.dp.tab) <(cut -f5- ${FN}.pq.tab) >  ${FN}.afs.dp.pq.tab
+  python ${BASEDIR}/Scripts/get_positions_from_sync.py -a ${FN}.afs.dp.pq.tab -b ${FN}_snpeff.tab --both | cat <(head -1  ${FN}.afs.dp.pq.tab )  -  >  ${FN}.afs.anno.tab ;
+  #
+  # sed ' s/DQ361065\.7/NP/g; s/DQ361065\.4/GP/g; s/DQ361066\.4/geneZ/g ; s/DQ361066\.7/geneL/g; s/\[[0-9]*\]//g' <  ${FN}.afs.anno.tab >  ${FN}.afs.anno_alt.tab ;
+  # sed 's/DQ361065\.7/NP/g; s/DQ361065\.4/GP/g; s/DQ361066\.4/geneZ/g ; s/DQ361066\.7/geneL/g; s/\[[0-9]*\]//g' <  ${FN}.stats.tab >  ${FN}.stats_alt.tab ;
+done
