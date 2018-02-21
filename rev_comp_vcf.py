e9dbe1e13449021b16fcad7bac09a49e7f89c147
Lukas Endler <lukasendler@i122mc94.vu-wien.ac.at>
2018-02-21 11:13:11 +0100

    changed revcop

--- rev_comp_vcf.py
+++ rev_comp_vcf.py
@@ -13,12 +13,15 @@ import os
 import argparse
 import gzip
 import json
+import string
 
 parser = argparse.ArgumentParser(description="""takes a VCF file and gives the reverse complement (for SNPgenie) to STDOUT   
 """)
 
 parser.add_argument("--in","-i", dest="vcffile", 
                     help="vcf-file, tries stdin if not set use \"STDIN\" or nothing for piping; default \"False\"", default=False)
+parser.add_argument("--snpgenie","-s", dest="snpgenie",
+                    help="input file is snpgenie result and give chromosome name default \"False\"", default=False)
 parser.add_argument("--chroms","-c", dest="chromlen",  type=str,
                     help="dict of chrom names to length; default \'{\"L\":7229,\"S\":3377\}\'", default='{"L":7229,"S":3377}')
 
@@ -26,6 +29,8 @@ args = parser.parse_args()
 # get chromlength
 chromlen=json.loads(vars(args)['chromlen'])
 vcf_file = vars(args)['vcffile']
+snpgenie = vars(args)['snpgenie']
+revtab=string.maketrans("ACTG","TGAC")
 # open vcf file
 if not vcf_file or vcf_file == "STDIN":
     if not sys.stdin.isatty():
@@ -43,11 +48,23 @@ for line in inf:
     if (re.match("^\s*\#+",line)): # entry is comment/header
         print line
         continue
-    entries = line.split("\t")
-    chrom = entries[0]
-    if not ("N" in entries[4]):
+    if (re.match("^\s*file",line) and snpgenie != False): # entry is snpgenie header
         print line
         continue
+    entries = line.split("\t")
+    if (snpgenie == False):
+        chrom = entries[0]
+        site = 1
+    else:
+        chrom = snpgenie
+        site = 2
+    entries[site] = str(chromlen[chrom] - int(entries[site]) + 1)
+    entries[3] = entries[3].translate(revtab)
+    entries[3] = entries[4].translate(revtab)
+    print "\t".join(entries)
+
+    
+    
 
 
 
